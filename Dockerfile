8316165a01c24e9bd3e45728f0b5c87360718850
Malachi Soord <inverse.chi@gmail.com>
2022-07-26 11:54:35 +0200

    Upgrade to Github Actions + Python version bump (#244)
    * Upgrade to Github Actions + Python version bump
    
    * Fix tests

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.6
+FROM python:3.10
 
 RUN \
   apt-get update && \
@@ -10,7 +10,8 @@ RUN \
 
 RUN mkdir -p /usr/src/app
 
-COPY . /usr/src/app
 WORKDIR /usr/src/app
+
+COPY . /usr/src/app
 
 CMD ["make"]
--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import pytest
 
 from haaska import HomeAssistant, Configuration
 
+
 @pytest.fixture
 def configuration():
     return Configuration(opts_dict={
@@ -13,23 +14,28 @@ def configuration():
         "ssl_client": []
     })
 
+
 @pytest.fixture
 def home_assistant(configuration):
     return HomeAssistant(configuration)
 
+
 def test_ha_build_url(home_assistant):
     url = home_assistant.build_url("test")
     assert url == "http://localhost:8123/api/test"
+
 
 def test_get_user_agent(home_assistant):
     os.environ["AWS_DEFAULT_REGION"] = "test"
     user_agent = home_assistant.get_user_agent()
     assert user_agent.startswith("Home Assistant Alexa Smart Home Skill - test - python-requests/")
 
+
 def test_config_get(configuration):
-    assert configuration.get(["debug"]) == False
-    assert configuration.get(["test"]) == None
+    assert configuration.get(["debug"]) is False
+    assert configuration.get(["test"]) is None
     assert configuration.get(["test"], default="default") == "default"
+
 
 def test_config_get_url(configuration):
     test_urls = [
